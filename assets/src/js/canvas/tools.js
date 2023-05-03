bf2d0ef12af5e82aa5857e4e28fc29a627618c70
Valeriia Postoronka <valeriia.postoronka@gmail.com>
2023-05-03 14:46:26 +0300

    Optimized tools.js and removed edit tools from tools array

--- assets/src/js/canvas/tools.js
+++ assets/src/js/canvas/tools.js
@@ -13,28 +13,29 @@ const toolsArray = [
     new tool('squareSolid', squareSolid),
     new tool('circleSolid', circleSolid),
     new tool('segment', segment),
-    new tool('fill', fill),
-    new tool('undo', undo),
-    new tool('redo', redo)
+    new tool('fill', fill)
 ];
 
 let activeTool = toolsArray[0];
 activeTool.func.addEvent();
 
-toolsDOMelements = document.getElementsByClassName('fa-xl');
+toolsDOMelementsCollection = document.getElementsByClassName('fa-xl');
+let toolsDOMelements =  [...toolsDOMelementsCollection];
+toolsDOMelements.pop();
+toolsDOMelements.pop();
 
-[...toolsDOMelements].forEach((element) => {
+toolsDOMelements.forEach((element) => {
     element.addEventListener('click', () => {
         eraseActive();
         eventListenerRemover();
-        activeTool = toolsArray[[...toolsDOMelements].indexOf(element)];
+        activeTool = toolsArray[toolsDOMelements.indexOf(element)];
         element.classList.add('active');
         activeTool.func.addEvent();
     });
 });
 
 let eraseActive = () => {
-    [...toolsDOMelements].forEach((element) => {
+    toolsDOMelements.forEach((element) => {
         element.classList.remove('active');
     });
 }
